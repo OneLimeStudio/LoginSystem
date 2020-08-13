15a6ec2912634f85efd80797d84ca76c2a55256a
OneLimeStudio <onelimestudio@gmail.com>
2020-08-13 21:03:48 +0530

    Added Register.py

--- register.py
+++ register.py
@@ -1,6 +1,20 @@
 import json
 
 
+database = open("main.txt","r+")
+
 def register():
     username = input(print("Plz tell the username"))
     
+    #if username in database.read():
+
+    with database as f:
+        if username in f.read():
+            print("This Username Already Exists")
+            f.close()
+
+        else:
+            f.write(" , " + username) 
+            print("Username Added !!")
+            f.close()
+
